2123c86c4f6101fac2c79ed9754ffd7a89974ff9
aut0luminescent <elge.paas@gmail.com>
2020-10-02 13:46:11 +0300

    2 files modified

--- src/app/api/api.component.ts
+++ src/app/api/api.component.ts
@@ -9,22 +9,20 @@ import { HttpClient } from '@angular/common/http';
 export class ApiComponent implements OnInit {
 
   apiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+  searchResult: any;
 
   @ViewChild('search') searchBox: ElementRef<HTMLInputElement>;
-  constructor(public http: HttpClient) { 
+  
+  constructor(public http: HttpClient) { }
 
-  }
-
-  ngOnInit() {
-   
-  }
+  ngOnInit() {}
 
 startSearch () {
-
     const searchTerm = this.searchBox.nativeElement.value;
 
     this.http.get( this.apiUrl + searchTerm ).subscribe((res)=> {
         console.log(res);
+        this.searchResult = res;
     })
   }
 }
